4323c3854eed00dd2102f7e73584e714badb1187
Rhyan <rhyan.randolph@activenetwork.com>
2017-12-19 05:36:34 -0600

    composer update remove WSDL classes

--- src/RunGenerator.php
+++ src/RunGenerator.php
@@ -6,7 +6,7 @@ use Dotenv\Dotenv;
 use Wsdl2PhpGenerator\Generator;
 use Wsdl2PhpGenerator\Config;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../autoload.php';
 
 RunGenerator::execute();
 
@@ -18,21 +18,32 @@ RunGenerator::execute();
 class RunGenerator
 {
     /**
-     *
+     * Generate WSDL classes
      *
      * @return void
      */
-    public static function execute() {
+    public static function execute()
+    {
         $dotenv = new Dotenv(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR);
         $dotenv->load();
 
         $generator = new Generator();
         $generator->generate(
            new Config(array(
-               'inputFile' => getenv('SELLIGENT_WSDL'),
+               'inputFile' => getenv('SOAP_WSDL'),
                'outputDir' => __DIR__.'/Selligent/MessageStudio',
                'namespaceName' => 'Selligent\MessageStudio'
            ))
         );
+    }
+
+    /**
+     * Test connection
+     *
+     * @return void
+     */
+    public static function test()
+    {
+
     }
 }
